6ae088dfe29434fd7e4cf0d92cca348791c9c394
SajibKingo <sksaha0167@gmail.com>
2024-11-05 15:10:46 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict";  // treat all JS code as newer version
+
+// alert(3+3)  // we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  // code redability should be high
+
+let name = "kingo"
+let age = 23
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint => for big number
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+// symbol => uniqueness
+
+// object
+
+console.log(typeof "kingo");
+console.log(typeof age);
+console.log(typeof null);  // type => object
+console.log(typeof undefined);  // type => undefined
