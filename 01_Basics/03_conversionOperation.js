39f7d82596e74987f597f326cbb72c0a48a944fc
SajibKingo <sksaha0167@gmail.com>
2024-11-05 16:44:04 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "kingo"
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)  // coversion
+console.log(typeof valueInNumber);
+console.log(valueInNumber);  // NaN => Not a Number
+
+// "45" => 45
+// "45abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "kingo" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
