d7da671799bb871669b0dfc3b20e9a29daa6baa2
SajibKingo <sksaha0167@gmail.com>
2024-11-14 09:52:54 +0000

    switch case

---
+++ 04_control_flow/2_switch.js
@@ -0,0 +1,29 @@
+// switch case syntax:
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 0:
+        console.log("jan");
+        break;
+    case 1:
+        console.log("feb");
+        break;
+    case 2:
+        console.log("march");
+        break;
+    case 3:
+        console.log("april");
+        break;
+
+    default:
+        console.log("Default case match");
+        break;
+}
