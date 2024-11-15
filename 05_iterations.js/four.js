fe425cb7ea70a43adf23bdcf2838ce2b32dfafe1
SajibKingo <sksaha0167@gmail.com>
2024-11-15 08:20:39 +0000

    for in

---
+++ 05_iterations.js/four.js
@@ -0,0 +1,32 @@
+// for in
+
+const myObject = {
+    js: "javascript",
+    py: "python",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+   //console.log(`${key} is shortcut for ${myObject[key]}`);
+}
+
+const programming = ["js", "py", "cpp", "rb", "swift"]
+
+for (const key in programming) {
+    //console.log(programming[key]);  
+}
+
+// const map = new Map()
+// map.set('In',"India")
+// map.set('Bd',"Bangladesh")
+// map.set('Fr',"France")
+// map.set('Bd',"Bangladesh")
+
+// console.log(map);
+
+// for (const [key, value] in map) {
+//     console.log(key, ":", value);  
+// }
+
